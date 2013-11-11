b4862bac20f46cca91003a72f17ce354574137bb
Andreas Madsen <amwebdk@gmail.com>
2013-11-11 22:39:04 +0100

    [test] add test for collecting in the not initial gc case

---
+++ test/simple/gc-not-initial.js
@@ -0,0 +1,145 @@
+
+var test = require('tap').test;
+var endpoint = require('endpoint');
+
+var setup = require('../setup.js')();
+var match = require('../match.js');
+
+setup.open({ maxage: [9, 6, 3] });
+
+var NOW_INITIAL = 0;
+var NOW_NOT_INITIAL = 0;
+
+var READER_REQUEST = {
+  'type': 'read-start',
+  'startSeconds': null,
+  'startMilliseconds': null,
+  'endSeconds': null,
+  'endMilliseconds': null,
+  'levels': [1, 9]
+};
+
+test('write testing log - initial', function (t) {
+  NOW_INITIAL =  Math.floor(Date.now() / 1000) - 2;
+
+  var writeRequest = {
+    'type': 'write',
+    'id': 0,
+    'seconds': NOW_INITIAL,
+    'milliseconds': 500,
+    'level': 3,
+    'message': new Buffer('level 3 - first')
+  };
+
+  setup.storage.write(writeRequest, function (err, response) {
+    t.equal(err, null);
+    t.deepEqual(response, {
+      'type': 'write',
+      'id': 0,
+      'error': null
+    });
+    t.end();
+  });
+});
+
+test('check that test log exists', function (t) {
+  setup.storage.reader(READER_REQUEST).pipe(endpoint({objectMode: true}, function (err, rows) {
+    t.equal(err, null);
+    t.equal(rows.length, 2);
+
+    match(t, rows[0], {
+      type: 'read-start',
+      level: 3,
+      seconds: NOW_INITIAL,
+      milliseconds: 500,
+      message: new Buffer('level 3 - first')
+    });
+
+    match(t, rows[1], {
+      type: 'read-stop',
+      error: null
+    });
+
+    t.end();
+  }));
+});
+
+test('initial log should be collected after 2 sec', function (t) {
+  setTimeout(function () {
+    setup.storage.reader(READER_REQUEST).pipe(endpoint({objectMode: true}, function (err, rows) {
+      t.equal(err, null);
+      t.equal(rows.length, 1);
+
+      match(t, rows[0], {
+        type: 'read-stop',
+        error: null
+      });
+
+      t.end();
+    }));
+  }, 1000 * 3);
+});
+
+test('write testing log - not initial', function (t) {
+  NOW_NOT_INITIAL =  Math.floor(Date.now() / 1000) - 2;
+
+  var writeRequest = {
+    'type': 'write',
+    'id': 0,
+    'seconds': NOW_NOT_INITIAL,
+    'milliseconds': 500,
+    'level': 3,
+    'message': new Buffer('level 3 - second')
+  };
+
+  setup.storage.write(writeRequest, function (err, response) {
+    t.equal(err, null);
+    t.deepEqual(response, {
+      'type': 'write',
+      'id': 0,
+      'error': null
+    });
+    t.end();
+  });
+});
+
+test('check that test log exists', function (t) {
+  setup.storage.reader(READER_REQUEST).pipe(endpoint({objectMode: true}, function (err, rows) {
+    t.equal(err, null);
+    t.equal(rows.length, 2);
+
+    match(t, rows[0], {
+      type: 'read-start',
+      level: 3,
+      seconds: NOW_NOT_INITIAL,
+      milliseconds: 500,
+      message: new Buffer('level 3 - second')
+    });
+
+    match(t, rows[1], {
+      type: 'read-stop',
+      error: null
+    });
+
+    t.end();
+  }));
+});
+
+test('none initial log should be collected after 2 sec', function (t) {
+  setTimeout(function () {
+    setup.storage.reader(READER_REQUEST).pipe(endpoint({objectMode: true}, function (err, rows) {
+      t.equal(err, null);
+      t.equal(rows.length, 1);
+
+      match(t, rows[0], {
+        type: 'read-stop',
+        error: null
+      });
+
+      t.end();
+    }));
+  }, 1000 * 3);
+});
+
+
+setup.close();
